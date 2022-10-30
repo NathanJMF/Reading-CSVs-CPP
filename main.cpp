6958fb2270adade418f41675c3bd2812b59e8ccc
nathanfenwick <nathanfenwick@rocketmail.com>
2022-10-30 12:34:34 +0400

    Added checking of passed arguments.

--- main.cpp
+++ main.cpp
@@ -2,14 +2,44 @@
 // Created by Nathan Fenwick on 30/10/2022.
 //
 #include <iostream>
+#include <fstream>
 
 using namespace std;
-
-
+bool check_fp(const string& fp);
+bool check_op(const string& op);
+/**
+How to run:
+g++ main.cpp -o main
+./main file_path operation_name
+**/
 int main(int argc, char** argv) {
-    cout << "You have entered " << argc << " arguments:" << endl;
-    for (int count = 0; count < argc; count++){
-        cout << argv[count] << endl;
+    // Check if valid number of arguments passed.
+    if (argc > 3){
+        cout << "Please pass a valid number of arguments" << endl;
+        return 0;
+    }
+
+    // Check to see if arg1 and arg2 are valid.
+    string path = argv[1];
+    string operation = argv[2];
+    if (!check_fp(path) || !check_op(operation)){
+        cout << "Please pass valid arguments" << endl;
+        return 0;
     }
     return 0;
+}
+
+bool check_fp(const string& fp){
+    ifstream check(fp);
+    if (!check){
+        return false;
+    }
+    return true;
+}
+
+bool check_op(const string& op){
+    if (op == "print" || op == "sum" || op == "mean" || op == "std"){
+        return true;
+    }
+    return false;
 }
